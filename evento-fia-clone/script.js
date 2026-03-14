fa2bfb4b54d60d8981956e292a8df01123a03ca1
3seatech-cyber <3.seatech@gmail.com>
2026-03-13 21:04:21 -0500

    Add files via upload

---
+++ evento-fia-clone/script.js
@@ -0,0 +1,12 @@
+const toggle = document.querySelector('.nav-toggle');
+const nav = document.querySelector('.site-nav');
+
+if (toggle && nav) {
+  toggle.addEventListener('click', () => {
+    nav.classList.toggle('is-open');
+  });
+
+  nav.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', () => nav.classList.remove('is-open'));
+  });
+}
